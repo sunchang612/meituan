bdc13b1c596bf7fd4da497be90f747f4dcd16e3f
sunchang1996 <sunchang612@gmail.com>
2019-03-18 22:06:59 +0800

    :sparkles: 首页静态页面

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,23 @@
 <template>
   <el-container class="layout-container">
-    <el-header>
+    <el-header style="height:100%">
       <my-header/>
     </el-header>
     <el-main>
       <nuxt />
     </el-main>
     <el-footer>
-      footer
+      <my-footer></my-footer>
     </el-footer>
   </el-container>
 </template>
 <script>
 import MyHeader from '~/components/public/header/index.vue'
+import MyFooter from '~/components/public/footer/index.vue'
 export default {
   components: {
-    MyHeader
-  }
+    MyHeader,
+    MyFooter
+  },
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,103 +1,37 @@
 <template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        pc-meituan
-      </h1>
-      <h2 class="subtitle">
-        My supreme Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
-    </div>
-    <el-button @click="handleClick">点击</el-button>
-  </section>
+  <div class="page-index">
+    <el-row>
+      <el-col :span="5">
+        <m-menu/>
+      </el-col>
+      <el-col :span="19">
+        <life/>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24">
+        <artistic></artistic>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
-
+import MMenu from '~/components/index/menu.vue'
+import Life from '@/components/index/life.vue'
+import Artistic from '@/components/index/artistic.vue'
 export default {
   components: {
-    Logo
+    Logo,
+    MMenu,
+    Life,
+    Artistic
   },
-  methods: {
-    handleClick() {
-      const h = this.$createElement;
-      this.$notify({
-        title: '提示',
-        message: h('div', null, [
-          '这是一个段文字',
-          h('a', 
-            { attrs: { 
-                href: 'https://www.jianshu.com/u/639201a5d397' , 
-                target: '_blank'
-              }
-            },
-            [
-              h('el-button', {
-                style: {
-                  marginLeft: '20px'
-                },
-                attrs: {
-                  size: 'small',
-                  type: 'primary'
-                }
-              }, '查看详情')
-            ]
-           ) 
-        ]),
-        duration: 0
-      });
-    }
-  }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-
+<style lang="scss">
+  @import "@/assets/css/index/index.scss";
 </style>
 
-<style lang="less">
-.links {
-  padding-top: 15px;
-}
-</style>
