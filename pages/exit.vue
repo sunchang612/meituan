01910ab4d977774fe766eb6fc563d7e101bc3b3b
sunchang1996 <sunchang612@gmail.com>
2019-03-31 15:52:55 +0800

    :sparkles: middleware exit

--- pages/exit.vue
+++ pages/exit.vue
@@ -5,5 +5,11 @@
 <script>
 export default {
   layout: 'blank',
+  middleware: async (ctx) => {
+    const { status, data } = await ctx.$axios.get('/users/exit')
+    if (status === 200 && data && data.code === 0) {
+      window.location.href='/'
+    }
+  }
 }
 </script>
